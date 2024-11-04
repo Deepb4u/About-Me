7820c8e735dde5d860e1f149aeec019993d09217
Deepb4u <deepbansode1@gmail.com>
2024-11-04 20:51:08 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors());
-app.use(cors({ origin: "https://client-dxfs.onrender.com" }));
+app.use(cors());
+// app.use(cors({ origin: "https://client-dxfs.onrender.com" }));
 app.use(bodyParser.json());
 
 // Chatbot logic: Simple responses
